d9510ed1b3defb98caf602157059112b5a4ac87a
Chad Lineberry <chad@tcgplayer.com>
2022-12-02 12:47:26 -0500

    Solving day 2

--- Day-02/day-02.dart
+++ Day-02/day-02.dart
@@ -1,11 +1,84 @@
+import 'dart:math';
 import 'package:aoc_2022/helpers.dart' as helpers;
 
-Future<String> part1() async {
-  var contents = helpers.readFileAsync('Day-01/part1.txt');
-  return contents;
+Future<int> part1() async {
+  var rounds = await helpers.readFileLinesAsync('Day-02/part1.txt');
+  var totalScore = 0;
+  for (var round in rounds) {
+    totalScore += calculateScore(round);
+  }
+  return totalScore;
 }
 
-Future<List<String>> part2() async {
-  var contents = helpers.readFileLinesAsync('Day-01/part2.txt');
-  return contents;
+Future<int> part2() async {
+  var rounds = await helpers.readFileLinesAsync('Day-02/part1.txt');
+  var totalScore = 0;
+  for (var round in rounds) {
+    totalScore += calculateShape(round);
+  }
+  return totalScore;
+}
+
+int calculateScore(String round) {
+  var score = 0;
+
+  if (round.endsWith("X")) {
+    score += 1;
+  }
+  if (round.endsWith("Y")) {
+    score += 2;
+  }
+  if (round.endsWith("Z")) {
+    score += 3;
+  }
+
+  switch (round) {
+    case "A X": //Ties
+    case "B Y":
+    case "C Z":
+      score += 3;
+      break;
+    case "A Y": //Wins
+    case "B Z":
+    case "C X":
+      score += 6;
+      break;
+  }
+  return score;
+}
+
+int calculateShape(String round) {
+  var score = 0;
+
+  var theirs = round.split(" ")[0];
+  var mine = round.split(" ")[1];
+
+  //Ties
+  if (mine == "Y") {
+    score += 3;
+  }
+  //Wins
+  if (mine == "Z") {
+    score += 6;
+  }
+
+  switch (round) {
+    case "A Y": //Rock
+    case "B X":
+    case "C Z":
+      score += 1;
+      break;
+    case "B Y": //Paper
+    case "C X":
+    case "A Z":
+      score += 2;
+      break;
+    case "C Y": //Scissor
+    case "A X":
+    case "B Z":
+      score += 3;
+      break;
+  }
+
+  return score;
 }
--- bin/aoc_2022.dart
+++ bin/aoc_2022.dart
@@ -1,9 +1,9 @@
-import '../Day-01/day-01.dart' as day_01;
+import '../Day-02/day-02.dart' as day_02;
 
 Future<void> main(List<String> arguments) async {
-  var results = await day_01.part1();
+  var results = await day_02.part1();
   print(results);
 
-  var results2 = await day_01.part2();
+  var results2 = await day_02.part2();
   print(results2);
 }
