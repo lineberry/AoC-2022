37eabbe8a01df5fe858d8606e8e43fbe0fd4be2d
Chad Lineberry <chad@tcgplayer.com>
2022-12-04 11:43:26 -0500

    sacrificing readability for fewer lines of code

--- Day-04/day-04.dart
+++ Day-04/day-04.dart
@@ -1,27 +1,11 @@
 import 'package:aoc_2022/helpers.dart' as helpers;
 
-class Assignment {
-  int range1Lower = 0;
-  int range1Upper = 0;
-  int range2Lower = 0;
-  int range2Upper = 0;
-
-  Assignment(String stringAssignment) {
-    var range1 = stringAssignment.split(",")[0];
-    var range2 = stringAssignment.split(",")[1];
-    range1Lower = int.parse(range1.split("-")[0]);
-    range1Upper = int.parse(range1.split("-")[1]);
-    range2Lower = int.parse(range2.split("-")[0]);
-    range2Upper = int.parse(range2.split("-")[1]);
-  }
-}
-
 Future<int> part1() async {
   var assignments = await helpers.readFileLinesAsync('Day-04/input.txt');
 
   return assignments
-      .map((a) => Assignment(a))
-      .map((ca) => doesAssignmentCompletelyOverlap(ca))
+      .map((e) => e.split(RegExp(r",|-")).map(int.parse).toList())
+      .map((e) => doesAssignmentCompletelyOverlap(e))
       .reduce((value, element) => value + element);
 }
 
@@ -29,28 +13,28 @@ Future<int> part2() async {
   var assignments = await helpers.readFileLinesAsync('Day-04/input.txt');
 
   return assignments
-      .map((a) => Assignment(a))
-      .map((ca) => doesAssignmentPartiallyOverlap(ca))
+      .map((e) => e.split(RegExp(r",|-")).map(int.parse).toList())
+      .map((e) => doesAssignmentPartiallyOverlap(e))
       .reduce((value, element) => value + element);
 }
 
-int doesAssignmentCompletelyOverlap(Assignment assignment) {
+int doesAssignmentCompletelyOverlap(List<int> assignmentBounds) {
   //Does 1 contain 2?
-  if (assignment.range1Lower <= assignment.range2Lower &&
-      assignment.range1Upper >= assignment.range2Upper) {
+  if (assignmentBounds[0] <= assignmentBounds[2] &&
+      assignmentBounds[1] >= assignmentBounds[3]) {
     return 1;
   }
   //Does 2 contain 1?
-  if (assignment.range2Lower <= assignment.range1Lower &&
-      assignment.range2Upper >= assignment.range1Upper) {
+  if (assignmentBounds[2] <= assignmentBounds[0] &&
+      assignmentBounds[3] >= assignmentBounds[1]) {
     return 1;
   }
   return 0;
 }
 
-int doesAssignmentPartiallyOverlap(Assignment assignment) {
-  if (assignment.range1Upper < assignment.range2Lower ||
-      assignment.range2Upper < assignment.range1Lower) {
+int doesAssignmentPartiallyOverlap(List<int> assignmentBounds) {
+  if (assignmentBounds[1] < assignmentBounds[2] ||
+      assignmentBounds[3] < assignmentBounds[0]) {
     return 0;
   }
   return 1;
